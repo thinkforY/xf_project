800b9179537a44d8540fa5af21b1d435311f3fa7
thinkforY <2321149783@qq.com>
2017-11-23 15:58:28 +0800

    修改view路径

--- index.php
+++ index.php
@@ -19,7 +19,10 @@ define('APP_DEBUG',True);
 
 // 定义应用目录
 define('APP_PATH','./Application/');
-
+// 定义试图模板
+define("TMPL_PATH", './Tpl/');
+//Runtime
+define("RUNTIME_PATH", './Runtime/');
 // 引入ThinkPHP入口文件
 require './ThinkPHP/ThinkPHP.php';
 
